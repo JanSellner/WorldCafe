02171d307038eaac3c413e8e19ed65c429f11ec1
Jan S. (Milania1) <git@milania.de>
2019-08-25 10:57:54 +0200

    Output radio buttons via flask instead of manually

--- server/forms.py
+++ server/forms.py
@@ -7,7 +7,7 @@ from flask_wtf.file import FileAllowed
 
 class InputDataForm(FlaskForm):
     n_groups = IntegerField('Number of groups', validators=[DataRequired()], widget=NumberInput(min=2), default=3)
-    selection_type = RadioField('Selection type', choices=[('text', ''), ('file', '')], validators=[DataRequired()])
+    selection_type = RadioField('Selection type', choices=[('text', 'Specify users manually'), ('file', 'Upload a file')], validators=[DataRequired()], default='text')
     users = TextAreaField('Users', description='One name per line')
     file = FileField('File', validators=[FileAllowed(['csv'])], description='CSV file')
     submit = SubmitField('Start calculation')
