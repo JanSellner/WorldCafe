ddaebd26cc0f601df2ee2f71f73adc7fcf286c09
Jan S. (Milania1) <git@milania.de>
2019-08-31 10:10:45 +0200

    New evaluation method which tries to produce more continuous error values

--- GroupEvaluation.py
+++ GroupEvaluation.py
@@ -14,57 +14,60 @@ class GroupEvaluation:
 
         self.others = np.empty(self.n_students, dtype=object)
 
-    def add_last_comb(self, combs: np.ndarray):
+    def add_last_comb(self, days: np.ndarray):
         # Mapping for the last day
-        last_comb = np.apply_along_axis(lambda x: np.setdiff1d(self.groups, x), axis=0, arr=combs)
+        last_day = np.apply_along_axis(lambda x: np.setdiff1d(self.groups, x), axis=0, arr=days)
 
-        return np.vstack([combs, last_comb])
+        return np.vstack([days, last_day])
 
-    def error_group_sizes(self, combs: np.ndarray):
-        errors = []
+    def error_group_sizes(self, days: np.ndarray):
+        counts = np.zeros((days.shape[0], len(self.groups)))
 
-        for comb in combs:
-            counts = np.unique(comb, return_counts=True)[1]
-            if len(counts) < len(self.groups):
-                # At least one slot did not get a student at all --> max error
-                errors.append(1)
+        # Count how many students each group has on each day
+        for i, day in enumerate(days):
+            counts_day = np.unique(day, return_counts=True)[1]
+            if len(counts_day) < len(self.groups):
+                # At least one slot did not get a student at all --> high error
+                return 10
 
-            # Use the difference to the optimal solution as error
-            diffs = np.abs(counts - self.opt_group_size)
-            # The weakest group size counts (all groups should have an equal number of members)
-            diffs = np.max(diffs)
-            # Norm to [0;1]
-            errors.append(diffs / self.opt_group_size)
+            counts[i, :] = counts_day
 
-        # The group sizes should be good on every day
-        return max(errors)
+        # Note: the counts values are currently not in the range [0; 1] and hence not directly comparable to the other measures. The counts values tend to be higher which means that they have a higher importance. This is probably not too bad since equal group sizes are favorable in general
 
-    def error_diffusion(self, combs: np.ndarray):
-        confusions = np.zeros(self.n_students)
+        # The group sizes should be as close as possible to the optimum
+        # The group sizes should be roughly equal
+        return np.mean(np.abs(counts - self.opt_group_size)) + np.std(counts)
 
+    def error_diffusion(self, days: np.ndarray):
+        meets_others = np.zeros(self.n_students)
+
+        # Count how many other students each student meets (including herself/himself)
         for i in range(self.n_students):
             others_indices = set()
 
-            for comb in combs:
+            for day in days:
                 # At each day, find the other team members (they are assigned to the same group number as the current student)
-                others_indices.update(np.where(comb == comb[i])[0])
+                others_indices.update(np.where(day == day[i])[0])
 
-            confusions[i] = len(others_indices)
+            meets_others[i] = len(others_indices)
             self.others[i] = others_indices  # For statistics
 
-        # The student with the lowest confusion counts (all should be high)
-        return 1 - np.min(confusions) / self.n_students
+        # Norm to [0; 1]
+        meets_others /= self.n_students
 
-    def error_foreigners(self, combs: np.ndarray):
-        errors = []
+        # The percentage of other students met should be as high as possible
+        # The percentages should be roughly equal
+        return 1 - np.mean(meets_others) + np.std(meets_others)
 
-        # TODO: rename comb to days?
-        for comb in combs:
-            entropies = []
+    def error_foreigners(self, days: np.ndarray):
+        entropies = np.zeros((days.shape[0], len(self.groups)))
+
+        for i, day in enumerate(days):
+            entropies_day = []
             for group in self.groups:
-                group_members_indices = np.where(comb == group)[0]
+                group_members_indices = np.where(day == group)[0]
                 if len(group_members_indices) == 0:
-                    entropies.append(0)
+                    entropies_day.append(0)
                     continue
 
                 foreigners_group = self.foreigners[group_members_indices]
@@ -77,21 +80,24 @@ class GroupEvaluation:
 
                 # The entropy is highest for the fraction (0.5, 0.5) which corresponds to a very well distributed group
                 # It is lowest for e.g. (1, 0) which means that one group consists e.g. only of foreigners
-                entropies.append(entropy(probabilities, base=2.0))
+                entropies_day.append(entropy(probabilities, base=2.0))
 
-            # All groups should be well distributed, i.e. only the lowest entropy counts
-            errors.append(1 - np.min(entropies))
+            entropies[i, :] = entropies_day
 
-        # The distribution should be good on every day
-        return max(errors)
+        # The entropy impurity measure has a maximum of 1 for (0.5, 0.5)
+        entropies = 1 - entropies
 
-    def error_total(self, combs: np.ndarray):
-        if combs.shape[0] == len(self.groups) - 1:
-            combs = self.add_last_comb(combs)
+        # The entropy values should be as low as possible
+        # The entropy values should be roughly equal
+        return np.mean(entropies) + np.std(entropies)
 
-        error = [self.error_group_sizes(combs), self.error_diffusion(combs)]
+    def error_total(self, days: np.ndarray):
+        if days.shape[0] == len(self.groups) - 1:
+            days = self.add_last_comb(days)
+
+        error = [self.error_group_sizes(days), self.error_diffusion(days)]
 
         if self.foreigners is not None:
-            error.append(self.error_foreigners(combs))
+            error.append(self.error_foreigners(days))
 
         return error
