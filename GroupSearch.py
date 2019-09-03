b25b1dfe352f6c994dbe71807c3c05ae0159160b
Jan S. (Milania1) <git@milania.de>
2019-09-03 21:45:23 +0200

    Fix possible degenerated solution

--- GroupSearch.py
+++ GroupSearch.py
@@ -41,7 +41,27 @@ class GroupSearch:
         # Random combination for all users (columns) and all days (rows)
         combs = np.stack([np.random.choice(self.groups, size=len(self.groups) - 1, replace=False) for _ in range(self.n_users)]).transpose()
 
-        error = self.gval.error_total(combs)
+    def _start_random_walk(self, seed):
+        if seed == 0:
+            # The randomly initialized starting configuration may lead to degenerated results in extreme settings like 9 users and 6 groups. For this case, it is possible that some groups don't get users at all. Since this is something we want to avoid, we add an evenly distributed starting configuration manually so that there is at least one configuration which fills each group
+            # The goal is to produce something like the following (4 users and 3 groups):
+            # day1: 1 1 2 3
+            # day2: 2 2 3 1
+            first_day = np.sort(np.resize(self.groups, self.n_users))
+            days = [first_day]
+
+            for g in range(1, len(self.groups) - 1):
+                prev_day = days[g - 1]
+                next_day = prev_day % (len(self.groups)) + 1
+                days.append(next_day)
+
+            days = np.asarray(days)
+        else:
+            np.random.seed(seed)
+            # Random combination for all users (columns) and all days (rows)
+            days = np.stack([np.random.choice(self.groups, size=len(self.groups) - 1, replace=False) for _ in range(self.n_users)]).transpose()
+
+        error = self.gval.error_total(days)
         last_improvement = -1
 
         for i in range(64):
@@ -50,14 +70,14 @@ class GroupSearch:
             # For the selected user, iterate over every possible group assignment
             for new_slots in list(itertools.permutations(self.groups, len(self.groups) - 1)):
                 # Temporarily assign the user to new groups (for all days)
-                combs_copy = combs.copy()
-                combs_copy[:, idx_user] = new_slots
+                days_copy = days.copy()
+                days_copy[:, idx_user] = new_slots
 
-                error_new = self.gval.error_total(combs_copy)
+                error_new = self.gval.error_total(days_copy)
                 if sum(error_new) < sum(error):
                     error = error_new
-                    combs = combs_copy
+                    days = days_copy
                     last_improvement = i
                     print(i, error)
 
-        return error, last_improvement, combs
+        return error, last_improvement, days
