6cc81452f1ded59655ec682878c55dc81521d36d
Jan S. (Milania1) <git@milania.de>
2019-09-03 21:45:44 +0200

    Add better json encoder

---
+++ JSONNumpyEncoder.py
@@ -0,0 +1,11 @@
+import numpy as np
+import json
+
+
+class JSONNumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        if isinstance(obj, np.int64):
+            return int(obj)
+        return json.JSONEncoder.default(self, obj)
