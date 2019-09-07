68b41bc1ded1dd7145ab6d04a3025da8d201c092
Jan S. (Milania1) <git@milania.de>
2019-09-07 15:22:35 +0200

    Add d3 bundle info

---
+++ d3_bundle/index.js
@@ -0,0 +1,16 @@
+// Based on this guide: http://www.golang.ir/src/cmd/vendor/github.com/google/pprof/third_party/d3/README.md?m=text
+// Just run npm install and copy the resulting d3.min.js to static/d3.bundle.min.js
+
+export {
+  select,
+  event,
+  mouse,
+} from "d3-selection";
+
+export {
+    format,
+} from "d3-format";
+
+export {
+    drag,
+} from "d3-drag";
