bd0c50e7abe522d92835db20d9bbbcbbe36bfa8d
Jan S. (Milania1) <git@milania.de>
2019-09-17 21:37:35 +0200

    Update group_optimal script

--- group_optimal.py
+++ group_optimal.py
@@ -1,25 +1,29 @@
 import itertools
 
 import numpy as np
-from GroupEvaluation import GroupEvaluation
+from group_allocation_python.GroupEvaluation import GroupEvaluation
 from multiprocessing import Pool, cpu_count
 from MeasureTime import MeasureTime
 
 
 def run(combs):
     combs = np.array(combs).transpose()
-    return sum(gval.error_total(combs))
+    return sum(gval.error_components(combs))
 
 
-n_students = 6
-groups = [1, 2, 3]
-foreigners = np.array([0, 0, 1, 1, 0, 0], np.int32)  #, 0, 1, 1, 1, 0, 1
-assert len(foreigners) == n_students
-gval = GroupEvaluation(groups, n_students, foreigners)
+n_students = 8
+groups = [0, 1, 2]
+# foreigners = np.array([0, 0, 1, 1, 0, 0], np.int32)  #, 0, 1, 1, 1, 0, 1
+# assert len(foreigners) == n_students
+gval = GroupEvaluation(groups, n_students)
 
 if __name__ == "__main__":
+    # Test every possible combination (works only for small problems)
+    time_per_comb = 23.27 / 279936
     comb_student = list(itertools.permutations(groups, len(groups) - 1))
-    print(f'Number of combinations to check: {len(comb_student) ** n_students}')
+    n_combs = len(comb_student) ** n_students
+    print(f'Number of combinations to check: {n_combs}')
+    print(f'Estimated time: {time_per_comb * n_combs} s')
 
     with MeasureTime():
         pool = Pool(cpu_count())
